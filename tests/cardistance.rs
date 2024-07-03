206f8d67a86720d77b85c9fa6290dfd0249cb8ec
0x676e67 <gngppz@gmail.com>
2024-07-03 20:09:58 +0800

    Update cardistance test

--- tests/cardistance.rs
+++ tests/cardistance.rs
@@ -1,19 +1,13 @@
+mod pre;
 use fs::onnx::Variant;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn test() {
+    let dir = "tests/data/cardistance";
     let args = Default::default();
-
     let predictor = fs::onnx::new_predictor(Variant::Cardistance, &args)
         .await
         .unwrap();
 
-    let image_file = std::fs::read(
-        "tests/data/cardistance/0a969ea283f3d76599e4a5d85aa8ca3db4dbf45ac8166ba8fe5915533e2c149b.jpg",
-    )
-    .unwrap();
-    let guess = predictor
-        .predict(image::load_from_memory(&image_file).unwrap())
-        .unwrap();
-    assert_eq!(guess, 7);
+    pre::test_predictor(dir, predictor);
 }
