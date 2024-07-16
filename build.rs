55e07a0095b9b94427e191a0930111dbd010aaf2
0x676e67 <gngppz@gmail.com>
2024-07-16 12:37:50 +0800

    Add build config

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Need this for CoreML. See: https://ort.pyke.io/perf/execution-providers#coreml
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-fapple-link-rtlib");
-
-    #[cfg(target_os = "linux")]
-    // print rustflags = [ "-Clink-args=-Wl,-rpath,\\$ORIGIN" ]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,\\$ORIGIN");
-}
